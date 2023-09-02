48dd6616e3a56a20c67b62d438f57481486e857b
mnv37 <mnv37@drexel.edu>
2023-09-02 14:57:11 -0500

    added js

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+let burgerBtn = document.querySelector("#burger-btn");
+
+let navContainer = document.querySelector( "#nav-container")
+
+let canISeeTheMenu = false;
+
+burgerBtn.addEventListener("click", () => {
+
+if(canISeeTheMenu == false) {
+
+console.log("Show Menu");
+
+navContainer.classList.add("showNavContainer");
+
+burgerBtn.classList.add("open");
+
+canISeeTheMenu = true;
+
+} else {
+
+navContainer.classList.remove("showNavContainer");
+
+burgerBtn.classList.remove("open");
+
+canISeeTheMenu = false;
+
+} } )
+
+ 
+
+navContainer.addEventListener("click", () => {
+
+navContainer.classList.remove ("showNavContainer") ;
+
+burgerBtn.classList.remove ("open");
+
+canISeeTheMenu = false;
+
+} )
